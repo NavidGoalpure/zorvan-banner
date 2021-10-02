6d45c5c8818ff7828a50c91ff7d32798d207d051
navidgoalpure <navid.goalpure@gmail.com>
2021-10-02 14:43:06 +0330

    bizantine generals edition

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,43 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavidPic from './images/navidPic.jpg';
 import './App.css';
+import StartSvg from './star';
 
 function App() {
   return (
     <div className='App'>
       <div className='App-header'>
-        <div className='green-line'></div>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
+        <div className='sidebar'>
+          <p className='sidebar-title'>ژنرال های بیزانسی</p>
+
+          <div
+            style={{
+              display: 'flex',
+              justifyContent: 'center',
+              marginTop: '1rem',
+            }}
+          >
+            <StartSvg />
+            <StartSvg />
+            <StartSvg />
+          </div>
+          <img src={NavidPic} className='avatar ' alt='logo' />
+        </div>
+        {/* <div className='green-line'></div> */}
         <div className='card'>
           <div className='card-inside'>
             {/* <div className='yellow-line'></div> */}
-            <p className='title'>پول های ابتدایی</p>
+            <p className='title'>مصاحبه با نوید گل پور</p>
+            <br />
+            <p className='title'>بنیانگذار وب سایت </p>
+            <p className='title'>www.hezarchiz.com </p>
           </div>
         </div>
-        <div className='yellow-line'></div>
-        <div className='footer'>
+        {/* <div className='yellow-line'></div> */}
+        {/* <div className='footer'>
           <div style={{ marginTop: '2rem' }}> قسمت اول</div>
           <div> اسپانسر: www.hezarchiz.com</div>
-        </div>
+        </div> */}
       </div>
     </div>
   );
