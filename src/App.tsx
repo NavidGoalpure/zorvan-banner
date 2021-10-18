9c620873ce4f4b7d8c38a964701128d87b193374
navidgoalpure <navid.goalpure@gmail.com>
2021-10-18 12:49:56 +0330

    comming soon01

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Mic from './images/mic.png';
+import NewLogo from './newLogo.svg';
 import './App.css';
 
 function App() {
@@ -7,19 +8,21 @@ function App() {
     <div className='App'>
       <div className='App-header'>
         <div className='green-line'></div>
-        {/* <img src={logo} className='App-logo' alt='logo' /> */}
-        <div className='card'>
-          <div className='card-inside'>
-            {/* <div className='yellow-line'></div> */}
-            <p className='title'>پول های ابتدایی</p>
-          </div>
+        <div className='card  blink-box'>
+          <div className='card-inside blink-box'></div>
+        </div>
+        <div className='card-text-container'>
+          <p className='title flicker'>زروان</p>
+          <p className='title'>روایت می‌کند </p>
+          <p className='title'>منتظر باشید...</p>
         </div>
         <div className='yellow-line'></div>
         <div className='footer'>
-          <img src={Mic} className='mic-logo' alt='logo' />
+          <img src={NewLogo} className='App-logo' alt='logo' />
+          {/* <img src={Mic} className='mic-logo' alt='logo' /> */}
           <div className='footer-content'>
-            <div style={{ marginTop: '2rem' }}> قسمت اول</div>
-            <div> اسپانسر: www.hezarchiz.com</div>
+            <div style={{ marginTop: '1rem' }}>پادکست زروان</div>
+            <div> خیلی خیلی زود</div>
           </div>
         </div>
       </div>
